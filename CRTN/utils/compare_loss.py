29d0fc631451b2987c4fef9416c4eb21c95ce5b5
Xyz <xyz961014@gmail.com>
2020-01-15 17:14:31 +0800

    freq and loss & bar plot

--- CRTN/utils/compare_loss.py
+++ CRTN/utils/compare_loss.py
@@ -6,6 +6,8 @@ import numpy as np
 from pprint import pprint
 from tqdm import tqdm
 import visdom
+sys.path.append("..")
+from data.wp_loader import WPDataset
 vis = visdom.Visdom()
 assert vis.check_connection()
 
@@ -14,10 +16,13 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--baseline_loss", type=str, help="location of baseline loss")
     parser.add_argument("--model_loss", type=str, help="location of model loss")
+    parser.add_argument("--data", type=str, help="location of data")
     parser.add_argument("--smooth_window", type=int, default=50, 
                         help="smooth window")
-    parser.add_argument("--func", type=int, choices=[0, 1], default=0, 
-                        help="function, 0 for stat bag of word loss, 1 for observe variance and loss of model")
+    parser.add_argument("--loss_window", type=float, default=0.1, 
+                        help="loss window")
+    parser.add_argument("--func", type=int, choices=[0, 1, 2], default=0, 
+                        help="function, 0 for stat bag of word loss, 1 for observe variance and loss of model, 2 for observe word freq and word loss, word freq derived from data")
     return parser.parse_args()
 
 def main(args):
@@ -29,8 +34,8 @@ def main(args):
                 'legend': ['loss difference'],
                 'showlegend': True,
                }
-        opts_prob = {
-                'legend': ['prob - 0.5'],
+        opts_bar = {
+                'legend': [],
                 'showlegend': True,
                }
         loss_dict = dict()
@@ -46,10 +51,10 @@ def main(args):
                 break
 
             # remove \n
-            base_ref = base_ref[:-1]
-            base_loss = base_loss[:-1]
-            model_ref = model_ref[:-1]
-            model_loss = model_loss[:-1]
+            base_ref = base_ref.strip()
+            base_loss = base_loss.strip()
+            model_ref = model_ref.strip()
+            model_loss = model_loss.strip()
 
             if base_ref == model_ref:
                 base_ref = base_ref.split()
@@ -70,15 +75,30 @@ def main(args):
 
         word_losses = loss_dict.items()
         word_losses = sorted(word_losses, key=lambda x: np.mean(x[1][0]))
-        #word_meanloss = [(x[0], np.mean(x[1][0]), np.mean(x[1][1])) for x in word_losses]
-        #word_meanloss = sorted(word_meanloss, key=lambda x: x[1])
 
-        # the smaller, the better
-        #word_diffs = [(x[0], np.mean(x[1][1]) - np.mean(x[1][0])) for x in word_losses]
-        #word_diffs = sorted(word_diffs, key=lambda x:x[1])
-        # the bigger, the better
-        #word_betters = [(x[0], x[1][2]) for x in word_losses]
-        #word_betters = sorted(word_betters, key=lambda x:x[1], reverse=True)
+        j = 0
+        loss_bars = []
+        x_labels = []
+        for i in range(round(word_losses[-1][1][0][0] / args.loss_window) + 1):
+            if j >= len(word_losses):
+                break
+            lossb_window = []
+            lossm_window = []
+            while np.mean(word_losses[j][1][0]) < (i + 1) * args.loss_window:
+                lossb = np.mean(word_losses[j][1][0])
+                lossb_window += word_losses[j][1][0]
+                lossm_window += word_losses[j][1][1]
+                j += 1
+                if j >= len(word_losses):
+                    break
+            if len(lossb_window) > 0:
+                loss_diff = np.mean(lossb_window) - np.mean(lossm_window)
+                loss_bars.append(loss_diff)
+            else:
+                loss_bars.append(0.)
+            x_labels.append(i * args.loss_window)
+
+        vis.bar(np.array(loss_bars), np.array(x_labels), win="loss bar")
 
         for i in tqdm(range(len(word_losses) - args.smooth_window + 1)):
             lossb = np.mean(word_losses[i][1][0])
@@ -96,7 +116,9 @@ def main(args):
             loss_diff = np.mean(loss_diff)
             prob_model_better = model_better / total
             vis.line(np.array([[loss_diff]]), np.array([lossb]), opts=opts_diff, win="loss diff", update="append")
-            vis.line(np.array([[prob_model_better - 0.5]]), np.array([lossb]), opts=opts_prob, win="prob of model better - 0.5", update="append")
+
+
+            
 
 
 
@@ -114,8 +136,8 @@ def main(args):
                 if model_ref.strip() == "":
                     break
 
-                model_loss = model_loss[:-1]
-                model_var = model_var[:-1]
+                model_loss = model_loss.strip()
+                model_var = model_var.strip()
 
                 model_loss = [float(l) for l in model_loss.split()]
                 model_var = [float(l) for l in model_var.split()]
@@ -128,7 +150,33 @@ def main(args):
                 loss = loss_var[i][0]
                 var = np.mean([x[1] for x in loss_var[i:i+args.smooth_window]])
                 vis.line(np.array([[var]]), np.array([[loss]]), win="loss-variance", update="append")
-        
+    elif args.func == 2:
+        freq_loss = []
+        corpus = WPDataset(args.data, 1e6, 50)
+        vocab = corpus.TEXT.vocab
+        with open(args.model_loss, "r") as model_file:
+            while True:
+                model_ref = model_file.readline()
+                model_loss = model_file.readline()
+                model_var = model_file.readline()
+                
+                if model_ref.strip() == "":
+                    break
+
+                model_ref = model_ref.strip()
+                model_loss = model_loss.strip()
+
+                model_ref = model_ref.split()
+                model_loss = [float(l) for l in model_loss.split()]
+
+                for l, r in list(zip(model_loss, model_ref)):
+                    freq_loss.append((r, vocab.freqs[r], l))
+            
+            freq_loss = sorted(freq_loss, key=lambda x:x[1])
+            for i in tqdm(range(0, len(freq_loss) - args.smooth_window + 1)):
+                freq = freq_loss[i][1]
+                loss = np.mean([x[2] for x in freq_loss[i:i+args.smooth_window]])
+                vis.line(np.array([[loss]]), np.array([[freq]]), win="freq-loss", update="append")
 
 
 if __name__ == "__main__":
