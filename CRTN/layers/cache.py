9af47389bf303dde43d86a0b831a1d9d4086d5c1
Xyz <xyz961014@gmail.com>
2020-02-20 15:30:36 +0800

    init_std removed in init_key

--- CRTN/layers/cache.py
+++ CRTN/layers/cache.py
@@ -64,7 +64,7 @@ class Cache(nn.Module):
                     for i in range(args.cache_N)
         })
 
-        self.init_keys(args.init_std, args.seed)
+        self.init_keys(args.seed)
 
         self.attn = DotProductAttention()
         if not args.no_summary:
@@ -99,10 +99,10 @@ class Cache(nn.Module):
         if self.demo:
             self.words = self.words.to(device)
 
-    def init_keys(self, init_std, seed=0):
+    def init_keys(self, seed=0):
         torch.manual_seed(seed)
         for i in range(self.mem_start, self.mem_end + 1):
-            nn.init.normal_(getattr(self, "key" + str(i)), std=init_std)
+            nn.init.normal_(getattr(self, "key" + str(i)))
 
     def _get_keys(self):
         keys = [getattr(self, "key" + str(i)) 
@@ -185,7 +185,7 @@ class Cache(nn.Module):
                             for i in range(self.N)
             })
 
-        self.init_keys(self.args.init_std, self.args.seed)
+        self.init_keys(self.args.seed)
 
         self.to(device)
 
