4d92521b43152574c5c063ddb9d4f93a1d06424e
Xyz <xyz961014@gmail.com>
2020-03-16 15:31:19 +0800

    sh change

--- CRTN/runfn.sh
+++ CRTN/runfn.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-
 python main.py \
     --datasets ptb \
     --adam \
     --epochs 200 \
     --lr 5e-4 \
+    --warmup_steps 5000 \
     --dropout 0.4 \
     --dropatt 0.4 \
     --weight_decay 1e-5 \
     --nlayers 12 \
     --d_ff 1024 \
-    --nhead 8 \
-    --emsize 256 \
-    --nhid 256 \
-    --batch_size 200 \
-    --eval_batch_size 200 \
+    --nhead 10 \
+    --emsize 400 \
+    --nhid 400 \
+    --batch_size 128 \
+    --eval_batch_size 128 \
     --num_steps 20 \
     --neighbor_len 20 \
     --cache_k 2 \
@@ -24,12 +24,13 @@ python main.py \
     --attn_type 1 \
     --seed 1111 \
     --adaptive \
+    --cutoffs 2000 4000 8000 \
     --no_summary \
     --wise_summary \
     --farnear \
     --query_method single_sum \
     --eval_steps 3000 \
-    --log-interval 50 \
+    --log-interval 100 \
     --distributed \
     ${@:1}
 
--- baseline/pytorch/run.sh
+++ baseline/pytorch/run.sh
@@ -5,24 +5,26 @@ python xl_lm.py \
     --adam \
     --epochs 200 \
     --lr 5e-4 \
+    --warmup_steps 5000 \
     --dropout 0.4 \
     --dropatt 0.4 \
     --weight_decay 1e-5 \
     --nlayers 12 \
     --d_ff 1024 \
-    --nhead 8 \
-    --emsize 256 \
-    --nhid 256 \
-    --batch_size 200 \
-    --eval_batch_size 200 \
+    --nhead 10 \
+    --emsize 400 \
+    --nhid 400 \
+    --batch_size 128 \
+    --eval_batch_size 128 \
     --num_steps 20 \
     --mem_len 60 \
     --tied \
     --attn_type 1 \
     --seed 1111 \
     --adaptive \
+    --cutoffs 2000 4000 8000 \
     --eval_steps 3000 \
-    --log-interval 50 \
+    --log-interval 100 \
     --distributed \
     --save base \
     ${@:1}
