e0f001670bc438a07e02b4f52f7183aa73dfe1e7
Xyz <xyz961014@gmail.com>
2020-04-13 22:48:18 +0800

    sh change

--- baseline/pytorch/run.sh
+++ baseline/pytorch/run.sh
@@ -3,31 +3,35 @@
 python xl_lm.py \
     --datasets ptb \
     --adam \
-    --std_epochs 150 \
+    --std_epochs 200 \
     --ema_epochs 50 \
-    --lr 4e-4 \
-    --eta_min 4e-5 \
-    --warmup_steps 5000 \
-    --dropout 0.5 \
-    --dropatt 0.2 \
-    --dropemb 0.2 \
-    --dropinp 0.6 \
-    --dropwei 0.3 \
+    --lr 8e-4 \
+    --eta_min 1e-4 \
+    --emb_mult 1 \
+    --ema_lr_mult 1 \
+    --alpha 0 \
+    --beta 0 \
+    --warmup_steps 20000 \
+    --dropout 0.6 \
+    --dropatt 0.3 \
+    --dropemb 0.15 \
+    --dropinp 0.65 \
+    --dropwei 0.05 \
     --dropfor 0.25 \
-    --weight_decay 12e-7 \
-    --nlayers 16 \
-    --d_ff 900 \
+    --weight_decay 1e-5 \
+    --nlayers 12 \
+    --d_ff 1024 \
     --nhead 10 \
-    --emsize 380 \
-    --nhid 380 \
-    --batch_size 20 \
+    --emsize 400 \
+    --nhid 400 \
+    --batch_size 40 \
     --eval_batch_size 10 \
-    --num_steps 70 \
-    --mem_len 70 \
+    --num_steps 80 \
+    --mem_len 80 \
     --tied \
     --seed 1111 \
     --adaptive \
-    --cutoffs 2000 4001 8000 \
+    --cutoffs 2000 4000 8000 \
     --eval_steps 3000 \
     --log-interval 200 \
     --distributed \
---
+++ baseline/pytorch/runptb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+python xl_lm.py \
+    --datasets ptb \
+    --adam \
+    --std_epochs 150 \
+    --ema_epochs 50 \
+    --lr 4e-4 \
+    --eta_min 4e-5 \
+    --warmup_steps 5000 \
+    --dropout 0.5 \
+    --dropatt 0.2 \
+    --dropemb 0.15 \
+    --dropinp 0.6 \
+    --dropwei 0.3 \
+    --dropfor 0.25 \
+    --weight_decay 12e-7 \
+    --nlayers 16 \
+    --d_ff 900 \
+    --nhead 10 \
+    --emsize 380 \
+    --nhid 380 \
+    --batch_size 20 \
+    --eval_batch_size 10 \
+    --num_steps 70 \
+    --mem_len 70 \
+    --tied \
+    --seed 1111 \
+    --adaptive \
+    --cutoffs 2000 4000 8000 \
+    --eval_steps 3000 \
+    --log-interval 200 \
+    --distributed \
+    --save base \
+    ${@:1}
