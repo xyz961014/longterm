98e221659152767f62f5775822ff2e2b549e4ce7
Xyz <xyz961014@gmail.com>
2020-10-28 16:49:44 +0800

    baseline mv

--- CRTN/utils/dist_scripts.py
+++ CRTN/utils/dist_scripts.py
@@ -1,14 +1,12 @@
 import sys
 sys.path.append("..")
 sys.path.append("../..")
-sys.path.append("../../baseline")
-sys.path.append("../../baseline/pytorch")
 from copy import copy
 import ipdb
 from story_tail import main as tail_main
 from main import main as lm_main
-from pytorch.xl_lm import main as base_lm_main
-from pytorch.story_tail import main as base_tail_main
+from baseline.pytorch.xl_lm import main as base_lm_main
+from baseline.pytorch.story_tail import main as base_tail_main
 
 def process_tail_fn(rank, args):
     local_args = copy(args)
